3483dd5f7df49f4b87cd36955b6f0ffb66ce2810
u.dukuev <u.dukuev@ase-ec.ru>
2024-09-22 17:43:26 +0300

    delete_element

--- delete_element.go
+++ delete_element.go
@@ -4,25 +4,33 @@ import "fmt"
 
 func removeElement(nums []int, val int) int {
 	nums_size := len(nums)
-	k, i, j := 0, 0, nums_size-1
-
-	for i < j {
-		if nums[i] == val {
-
-			for i < j && nums[j] == val {
-				j--
-			}
-			nums[i], nums[j] = nums[j], nums[i]
+	k := 0
+	if nums_size == 1 {
+		if nums[0] == val {
+			k = 0
 		} else {
-			i++
-			k++
+			k = 1
 		}
-	}
-	fmt.Println(k)
-	for i := 0; i < k; i++ {
-		fmt.Printf("%d ", nums[i])
-	}
+	} else {
+		i, j := 0, nums_size-1
 
+		for i < j {
+			if nums[i] == val {
+
+				for i < j && nums[j] == val {
+					j--
+				}
+				nums[i], nums[j] = nums[j], nums[i]
+			} else {
+				i++
+				k++
+			}
+		}
+		fmt.Println(k)
+		// for i := 0; i < k; i++ {
+		// 	fmt.Printf("%d ", nums[i])
+		// }
+	}
 	return k
 }
 
