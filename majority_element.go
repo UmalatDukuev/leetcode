2087d9ff9d19bbb9aac9e3fd3c9a97169cae4b08
u.dukuev <u.dukuev@ase-ec.ru>
2024-10-03 00:02:02 +0300

    majority_element

---
+++ majority_element.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func majority_element(nums []int) int {
+	curr, cnt := 0, 0
+
+	for _, num := range nums {
+		if cnt == 0 {
+			curr = num
+			cnt++
+		} else if curr == num {
+			cnt++
+		} else {
+			cnt--
+		}
+	}
+	return curr
+}
+
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 1, 2, 3, 3}
+	fmt.Println(majority_element(nums))
+}
