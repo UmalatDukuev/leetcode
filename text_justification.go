6b48f2e26f0308a0002db3a7ff5a5e471e01c3ea
Umalat <dukuev037@mail.ru>
2024-12-20 02:00:06 +0300

    text_justification.go

--- text_justification.go
+++ text_justification.go
@@ -5,12 +5,42 @@ import (
 )
 
 func fullJustify(words []string, maxWidth int) []string {
+	spaces := make([]int, 1)
+	strLen := 0
+	cnt := 0
+	for ind, val := range words {
+		strLen += len(val) + 1
+		cnt += 1
+		if strLen > maxWidth {
+			fmt.Println(strLen)
+			fmt.Println(val)
 
-	return []string{"qwe", "123"}
+			cnt = 0
+			strLen = 0
+			spaces = append(spaces, ind)
+		}
+	}
+	// st := 0
+	// for st < maxWidth {
+	// 	st += len(val)
+	// }
+	// strings := make([]string, 0)
+	// substr := ""
+	// fmt.Println(spaces)
+	// for ind, _ := range spaces {
+	// 	fmt.Println(ind)
+	// 	for i := int(spaces[ind]); i < int(spaces[ind+1])-1; i++ {
+	// 		substr += words[i]
+	// 	}
+	// 	strings = append(strings, substr)
+	// }
+	// fmt.Println(strings)
+
+	return []string{}
 }
 
 func main() {
-	words := []string{"This", "is", "an", "example", "of", "text", "justification."}
-	maxWidth := 16
+	words := []string{"Science", "is", "what", "we", "understand", "well", "enough", "to", "explain", "to", "a", "computer.", "Art", "is", "everything", "else", "we", "do"}
+	maxWidth := 20
 	fmt.Println(fullJustify(words, maxWidth))
 }
