a2244a5a486e38e46496419237917e9c15aa574d
Umalat <dukuev037@mail.ru>
2024-12-20 02:33:13 +0300

    в процессе

--- text_justification.go
+++ text_justification.go
@@ -8,44 +8,37 @@ func fullJustify(words []string, maxWidth int) []string {
 	spaces := make([]int, 1)
 	wordsLen := len(words)
 	strLen := 0
-	cnt := 0
-	for _, val := range words {
-		cnt += len(val)
-		fmt.Printf("%s: %d\n", val, len(val))
-		//fmt.Printf("Total %d\n", cnt)
-
-	}
-	fmt.Println("|||||||||||||||||||||||||||||||||||||||")
 
 	for i := 0; i < wordsLen; i++ {
 		strLen += len(words[i]) + 1
-		cnt += 1
 		if strLen > maxWidth+1 {
-			fmt.Println(strLen)
-			fmt.Println(words[i])
-
-			cnt = 0
 			strLen = 0
 			spaces = append(spaces, i)
 			i--
 		}
 	}
 	fmt.Println(spaces)
-	// st := 0
-	// for st < maxWidth {
-	// 	st += len(val)
-	// }
-	// strings := make([]string, 0)
+
+	result_strings := make([]string, 0)
 	// substr := ""
-	// fmt.Println(spaces)
-	// for ind, _ := range spaces {
-	// 	fmt.Println(ind)
-	// 	for i := int(spaces[ind]); i < int(spaces[ind+1])-1; i++ {
-	// 		substr += words[i]
-	// 	}
-	// 	strings = append(strings, substr)
-	// }
-	// fmt.Println(strings)
+
+	for i := 0; i < len(spaces)-1; i++ {
+
+		j := int(spaces[i])
+		for j < int(spaces[i+1]) {
+			fmt.Print(words[j])
+			fmt.Print(" ")
+
+			// substr += words[j] + " "
+			j++
+		}
+		fmt.Println("")
+
+		// substr += words[j+1]
+		// result_strings = append(result_strings, substr)
+	}
+
+	fmt.Println(result_strings)
 
 	return []string{}
 }
