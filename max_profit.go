374f9ab359920734da24f270df45b54dadf8e05f
Umalat <dukuev037@mail.ru>
2024-12-26 00:42:43 +0300

    max_profit2.go

---
+++ max_profit.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	i := 0
+	profit := 0
+	arrLen := len(prices)
+	for i < arrLen-1 {
+		currPrice := prices[i]
+		if prices[i+1] > currPrice {
+			profit += prices[i+1] - currPrice
+			// } else if prices[i] < currPrice{
+
+		}
+		i++
+	}
+	return profit
+}
+
+func main() {
+	prices := []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(prices))
+}
