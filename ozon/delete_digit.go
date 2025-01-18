dc52d573cee981773141e27753e3f6bd7918e8cd
Umalat <dukuev037@mail.ru>
2025-01-18 03:31:05 +0300

    удалить цифру из числа(озон)

--- ozon/delete_digit.go
+++ ozon/delete_digit.go
@@ -5,26 +5,44 @@ import (
 	"strconv"
 )
 
-func deleteMinInNumber(num int) int {
-	if num/10 == 0 {
-		return 0
-	}
-	min := 100
-	k := 0
+// func deleteMinInNumber(num int) int {
+// 	if num/10 == 0 {
+// 		return 0
+// 	}
+// 	min := 100
+// 	k := 0
+// 	n := num
+// 	for i := 0; num > 0; i++ {
+// 		p := num % 10
+// 		if p <= min {
+// 			min = p
+// 			k = i
+// 		}
+// 		num /= 10
+// 	}
+// 	st := strconv.Itoa(n)
+// 	le := len(st)
+// 	st = st[:(le-k-1)] + st[(le-k):]
+// 	n, _ = strconv.Atoi(st)
+// 	return n
+// }
+
+func findMax(num int) int {
 	n := num
-	for i := 0; num > 0; i++ {
-		p := num % 10
-		if p <= min {
-			min = p
-			k = i
-		}
-		num /= 10
-	}
 	st := strconv.Itoa(n)
 	le := len(st)
-	st = st[:(le-k-1)] + st[(le-k):]
-	n, _ = strconv.Atoi(st)
-	return n
+	numbers := make([]int, le)
+	for i := 0; i < le; i++ {
+		st := strconv.Itoa(n)
+		// st = st[:(le-i-1)] + st[(le-i):]
+		st = st[:(le-i-1)] + st[(le-i):]
+		n, _ = strconv.Atoi(st)
+		numbers[i] = n
+	}
+	// st = st[:(le-k-1)] + st[(le-k):]
+	// n, _ = strconv.Atoi(st)
+	fmt.Println(numbers)
+	return numbers[0]
 }
 
 func main() {
@@ -39,10 +57,10 @@ func main() {
 	// for i := 0; i < a; i++ {
 	// 	fmt.Fscan(in, &numbers[i])
 	// }
-	numbers := []int{987241, 3233, 3432}
+	numbers := []int{984724}
 	// max := -1
 	for i, val := range numbers {
-		numbers[i] = deleteMinInNumber(val)
+		numbers[i] = findMax(val)
 		// if numbers[i] > max {
 		// 	max = numbers[i]
 		// }
