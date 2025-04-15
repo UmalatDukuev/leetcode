6ab0178e51bc61722d17182b1f32a46e4ce20bd2
Umalat <dukuev037@mail.ru>
2025-04-16 01:24:11 +0300

    ввод для 3 банков

---
+++ ozon/three_banks.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+type Transfer struct {
+	From int
+	To   int
+}
+
+type Bank struct {
+	RublesToDollars Transfer
+	RublesToEuros   Transfer
+	DollarsToRubles Transfer
+	DollarsToEuros  Transfer
+	EurosToRubles   Transfer
+	EurosToDollars  Transfer
+}
+
+func findMaxDollar(banks []Bank) float32 {
+	return 0.015
+}
+
+func main() {
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	bankA := Bank{
+		RublesToDollars: Transfer{From: 100, To: 1},
+		RublesToEuros:   Transfer{From: 100, To: 1},
+		DollarsToRubles: Transfer{From: 1, To: 100},
+		DollarsToEuros:  Transfer{From: 3, To: 2},
+		EurosToRubles:   Transfer{From: 1, To: 100},
+		EurosToDollars:  Transfer{From: 2, To: 3},
+	}
+
+	bankB := Bank{
+		RublesToDollars: Transfer{From: 100, To: 1},
+		RublesToEuros:   Transfer{From: 100, To: 1},
+		DollarsToRubles: Transfer{From: 1, To: 100},
+		DollarsToEuros:  Transfer{From: 3, To: 2},
+		EurosToRubles:   Transfer{From: 1, To: 100},
+		EurosToDollars:  Transfer{From: 2, To: 3},
+	}
+
+	bankC := Bank{
+		RublesToDollars: Transfer{From: 100, To: 1},
+		RublesToEuros:   Transfer{From: 100, To: 1},
+		DollarsToRubles: Transfer{From: 1, To: 100},
+		DollarsToEuros:  Transfer{From: 3, To: 2},
+		EurosToRubles:   Transfer{From: 1, To: 100},
+		EurosToDollars:  Transfer{From: 2, To: 3},
+	}
+	banks := []Bank{bankA, bankB, bankC}
+	result := findMaxDollar(banks)
+	println(result)
+}
